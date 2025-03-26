d5dcfe4f4b65e07bce1693caa60c0d1d27df4ee1
Marharyta <rita.shyshkavets@gmail.com>
2025-03-26 12:27:35 +0100

    feat: complete memoize implementation

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -110,8 +110,12 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedResult = null;
+  return function memoizedResultFunction() {
+    if (cachedResult === null) cachedResult = func();
+    return cachedResult;
+  };
 }
 
 /**
