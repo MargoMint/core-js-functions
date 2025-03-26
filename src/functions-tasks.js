67bbf1f3f7c9feae71ac12bba6c596b6999f0907
Marharyta <rita.shyshkavets@gmail.com>
2025-03-26 16:12:58 +0100

    feat: complete getIdGeneratorFunction implementation

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -217,8 +217,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let newStartFrom = startFrom;
+  return function idGenerator() {
+    const currentId = newStartFrom;
+    newStartFrom += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
